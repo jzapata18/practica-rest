d0de12b2f03747117ce3b8955c054619afdb499d
jzapata18 <john.jzapata18@gmail.com>
2019-02-02 11:52:29 -0300

    avance rest

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_123636) do
+ActiveRecord::Schema.define(version: 2019_02_02_142028) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sex"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+User.delete_all
+
 5.times do |index|
 	User.create!(name: "user #{index}", age: 33)
 end
